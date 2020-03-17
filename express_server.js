2972f8f7995782dc473812e75144ba9baf07f524
Jaybur1 <jayburbyga@gmail.com>
2020-03-16 20:23:21 -0400

    implemented generetrendom string function to create our coresponded shortUrl for the new longUrl

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
+const { generateRandomString } = require('./factories');
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDataBase = {
@@ -10,6 +13,7 @@ const urlDataBase = {
 };
 
 app.get("/", (req, res) => {
+  
   res.send("Hello!");
 });
 
@@ -18,12 +22,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", tamplateVars);
 });
 
-app.get("/urls/:shortUrl", (req, res) => {
-  const shortUrl = req.params.shortUrl;
-  const longUrl = urlDataBase[shortUrl];
-  const tamplateVars = { shortUrl, longUrl};
+app.post("/urls", (req, res) => {
+  urlDataBase[generateRandomString()] = req.body.longURL; // Log the POST request body to the console
+  res.json(urlDataBase);         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new');
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDataBase[shortURL];
+  const tamplateVars = { shortURL, longURL };
   res.render("url_show", tamplateVars);
 });
+
+
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
---
+++ factories.js
@@ -0,0 +1,27 @@
+const randomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const generateRandomString = () => {
+  let randomKey = "";
+  for (let i = 0; i < 6; i++) {
+    const randomCase = randomNum(0, 3);
+    if (randomCase === 0) {
+      randomKey += randomNum(0, 9);
+    } else if (randomCase === 1) {
+      randomKey += String.fromCharCode(randomNum(65, 90));
+    } else {
+      randomKey += String.fromCharCode(randomNum(97, 122));
+    }
+  }
+  return randomKey;
+};
+
+console.log(generateRandomString());
+console.log(generateRandomString());
+console.log(generateRandomString());
+console.log(generateRandomString());
+console.log(generateRandomString());
+console.log(generateRandomString());
+console.log(generateRandomString());
+module.exports = { generateRandomString };
