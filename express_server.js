adae5daaee7fbc59e7f3aa23de3fccf21b669133
Jaybur1 <jayburbyga@gmail.com>
2020-03-17 20:03:55 -0400

    added login/logout functionality + saving login preference to cookies

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const { generateRandomString } = require("./factories");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const urlDataBase = {
@@ -16,22 +18,33 @@ app.get("/", (req, res) => {
   res.redirect("/u");
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/u");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/u');
+});
 app.get("/u", (req, res) => {
-  const tamplateVars = { urls: urlDataBase };
+  const tamplateVars = { urls: urlDataBase, username: req.cookies.username };
   res.render("urls_index", tamplateVars);
 });
 
 app.post("/u", (req, res) => {
   const shortURL = generateRandomString();
-  const longURL = req.body.longURL.substr(0,4) !== "http" ? `http://${req.body.longURL}` : req.body.longURL;
+  const longURL =
+    req.body.longURL.substr(0, 4) !== "http"
+      ? `http://${req.body.longURL}`
+      : req.body.longURL;
   urlDataBase[shortURL] = longURL; // Log the POST request body to the console
   res.redirect(`/u`); // Respond with 'Ok' (we will replace this)
 });
 
-
-
 app.get("/u/new", (req, res) => {
-  res.render("urls_new");
+  const username = { username: req.cookies.username };
+  res.render("urls_new", username);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -41,14 +54,29 @@ app.get("/u/:shortURL", (req, res) => {
     res.statusCode = 404;
     res.send("404, Page Not Found");
   } else {
-    // const tamplateVars = { shortURL, longURL };
     res.redirect(longURL);
   }
 });
 
-app.post("/u/:shortURL/delete", (req,res) => {
+app.get("/u/:shortURL/update", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDataBase[shortURL];
+  const tamplateVars = { shortURL, longURL, username: req.cookies.username };
+
+  res.render("url_show", tamplateVars);
+});
+
+app.post("/u/:shortURL/update", (req, res) => {
+  urlDataBase[req.params.shortURL] =
+    req.body.longURL.substr(0, 4) !== "http"
+      ? `http://${req.body.longURL}`
+      : req.body.longURL;
+  res.redirect("/u");
+});
+
+app.post("/u/:shortURL/delete", (req, res) => {
   delete urlDataBase[req.params.shortURL];
-  res.redirect('/u');
+  res.redirect("/u");
 });
 
 app.listen(PORT, () => {
